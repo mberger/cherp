ea4cec55c8c85942fcf8488342181942d61186ca
Michael Berger <mberger@internetinnovation.com>
2012-02-26 00:59:47 -0500

    Change-Id: Ief86e18e3778dded33de3c35e9e3084f80cc12eb

--- db/db_info.php
+++ db/db_info.php
@@ -2,5 +2,5 @@
 	define ('DB', database_name);
 	define ('DB_USER', database_user_name);
 	define ('DB_PASSWORD', 'database_password');
-	define ('DB_HOST', database_host);	
+	define ('DB_HOST', db_host);	
 ?>
