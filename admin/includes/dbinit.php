5b9a55780b7d6aae6bd03112246dda8d4151c08f
Michael Berger <mberger@internetinnovation.com>
2012-02-25 23:15:18 -0500

    Change-Id: I71442e81287678553807178c22bb4df5078f673e

--- admin/includes/dbinit.php
+++ admin/includes/dbinit.php
@@ -11,7 +11,7 @@
 
 	// Initialise database object and establish a connection
 	// at the same time - db_user / db_password / db_name / db_host
-	$db = new ezSQL_mysql('cominter_140','140dev','cominter_140','localhost');
+	$db = new ezSQL_mysql('db_user','db_password','db_name','db_host');
 
 	/**********************************************************************
 	*  ezSQL demo for mySQL database
