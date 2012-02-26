64afdcdbe358a288f20ca3f2e40fec5bb0c84633
Michael Berger <mberger@internetinnovation.com>
2012-02-25 23:28:19 -0500

    Change-Id: I42881d549b6266a399d5bd0e52df566e65126d1f

--- admin/processkeywords.php
+++ admin/processkeywords.php
@@ -5,6 +5,6 @@
 
 	//$db->query("UPDATE keywords SET keywords='$_POST[keywords]' WHERE id='1')");
 	//$db->query("update keywords set keywords = 'walt disney,wdw,lion king,pumba,anther one' where id=3");
-	$db->query("update keywords set keywords = '$_POST[keywords]' where id=N");
+	$db->query("update keywords set keywords = '$_POST[keywords]' where id=X");
 
 ?>
